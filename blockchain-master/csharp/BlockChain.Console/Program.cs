1711102870594a08c7634baa435f8d0d1111d846
Severi <32543075+Severi@users.noreply.github.com>
2018-02-18 23:49:55 +0200

    working connection string and ssl fix

--- blockchain-master/csharp/BlockChain.Console/Program.cs
+++ blockchain-master/csharp/BlockChain.Console/Program.cs
@@ -14,14 +14,14 @@ namespace BlockChainDemo.Console
             var chain = new BlockChain();
             var server = new WebServer(chain);
 
-            string connStr = "server=codez.savonia.fi;user=p22018kg5;database=projekti2_2018_kevat_group5;port=3306;password=p22018kg5";
+            string connStr = "server = 160.153.129.223; user id = bloblo;Pwd=bloblo; database = bloblo;encrypt = no ";
            MySqlConnection conn = new MySqlConnection(connStr);
             try
             {
                 System.Console.WriteLine("Connecting to MySQL...");
                 conn.Open();
 
-                string sql = "SELECT * from asd where 1";
+                string sql = "SELECT * from example where 1";
                 MySqlCommand cmd = new MySqlCommand(sql, conn);
                 MySqlDataReader rdr = cmd.ExecuteReader();
 
