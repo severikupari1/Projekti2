7528050b8db3c3d734391a5af17271f6b207164e
severi <severikupari1@gmail.com>
2018-02-28 13:49:40 +0200

    Revert "Revert "Database connection class""
    This reverts commit b8dcacc6f6cd5136b1d00684fefb18bb1410bb50.

---
+++ blockchain-master/csharp/BlockChain/DatabaseConnection.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Web;
+using System.Data;
+
+namespace BlockChainDemo
+{
+    public class DatabaseConnection
+    {
+        private string ConnectionString = "";
+        SqlConnection con;
+
+        
+
+        public void OpenConnection(string ConnectionString)
+        {
+            SqlConnection con;
+            con = new SqlConnection(ConnectionString);
+            con.Open();
+        }
+
+        public void CloseConnection()
+        {
+            con.Close();
+        }
+
+        public void ExecuteQueries(string Query_)
+        {
+            SqlCommand cmd = new SqlCommand(Query_, con);
+            cmd.ExecuteNonQuery();
+        }
+
+        public SqlDataReader DataReader(string Query_)
+        {
+            SqlCommand cmd = new SqlCommand(Query_, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            return dr;
+        }
+
+
+        public object ShowDataInGridView(string Query_)
+        {
+            SqlDataAdapter dr = new SqlDataAdapter(Query_, ConnectionString);
+            DataSet ds = new DataSet();
+            dr.Fill(ds);
+            object dataum = ds.Tables[0];
+            return dataum;
+        }
+
+
+    }
+}
