ee770329434aa81c304281ab5279995a7dd514e9
Severi <severikupari1@gmail.com>
2018-03-02 10:10:58 +0200

    references added and Database.cs made

---
+++ blockchain-master/csharp/BlockChain/DataBase.cs
@@ -0,0 +1,46 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChainDemo
+{
+    class DataBase
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string userid;
+        private string password;
+        private string encrypt;
+       // string connStr = "server = codez.savonia.fi;Pwd=p22018kg5; user id = p22018kg5; database = projekti2_2018_kevat_group5;encrypt = no";
+
+
+
+        private void InitializeConnection() {
+            server = "codez.savonia.fi";
+            database = "projekti2_2018_kevat_group5";
+            userid = "p22018kg5";
+            password = "p22018kg5";
+            encrypt = "no";
+
+            string connectionString;
+
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "user id=" + userid + ";" + "PASSWORD=" + password + ";" + "encrypt=" + encrypt + ";";
+
+            connection = new MySqlConnection(connectionString);
+
+        }
+
+
+        public void OpenConnection()
+        {
+
+        }
+       
+
+    }
+}
