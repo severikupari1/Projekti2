10c240004d48b1a8e52bcbb37ad9ce9fb0930892
Severi <severikupari1@gmail.com>
2018-03-02 10:14:26 +0200

    ConnectionOpen and ConnectionClosed methods added

--- blockchain-master/csharp/BlockChain/DataBase.cs
+++ blockchain-master/csharp/BlockChain/DataBase.cs
@@ -36,11 +36,54 @@ namespace BlockChainDemo
         }
 
 
-        public void OpenConnection()
+        //open connection to database
+        private bool OpenConnection()
         {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server.  Contact administrator"); 
+                        break;
 
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
         }
-       
+
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+
+
+
 
     }
 }
