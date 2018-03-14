5dc473f02de8a663e46fb0bc27f65b77692809de
seontonppa <tomimonkkonen2@gmail.com>
2018-03-14 12:24:54 +0200

    UI muutoksia
    Teeman värit, input kentät ja kirjautumisnappi

---
+++ user-interfaces/scripts/script.js
@@ -0,0 +1,8 @@
+function Login()
+{
+	var user = document.getElementById('login_username').value;
+	var pass = document.getElementById('login_password').value;
+	
+	// This alert should be replaced with a proper login check
+	alert('You logged in with username "' + user + '" and password "' + pass + '".');
+}
