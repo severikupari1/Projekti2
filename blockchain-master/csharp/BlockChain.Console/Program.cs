0793173b8e324cfec5b993e6664fbc10b37564b7
Severi Cu <severikupari1@gmail.com>
2018-03-28 19:06:46 +0300

    mongodb

--- blockchain-master/csharp/BlockChain.Console/Program.cs
+++ blockchain-master/csharp/BlockChain.Console/Program.cs
@@ -8,24 +8,28 @@ using System.Configuration;
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 
 namespace BlockChainDemo.Console
 {
+
     class Program
     {
 
-        
+
 
 
         static void Main(string[] args)
         {
             var chain = new BlockChain();
-            
+
             var s = chain.GetFullChain();
             System.Console.WriteLine(s);
 
             System.IO.File.WriteAllText("file.json", s);
-           
+
 
             var readS = System.IO.File.ReadAllText("file.json");
             BlockChain readChain = Newtonsoft.Json.JsonConvert.DeserializeObject<BlockChain>(readS);
@@ -35,71 +39,35 @@ namespace BlockChainDemo.Console
 
             // string connStr = "server = codez.savonia.fi;Pwd=p22018kg5; user id = p22018kg5; database = projekti2_2018_kevat_group5;encrypt = no";
 
-            var connectionDb = new DataBase();
+            //var connectionDb = new DataBase();
 
-            if (connectionDb.OpenConnection())
-            {
-                System.Console.WriteLine("Yhisty");
-            }
-            
-            
-            
-
-            //string connStr = "server = 160.153.129.223; user id = bloblo;Pwd=bloblo; database = bloblo;encrypt = no ";
-            
-            //MySqlConnection conn = new MySqlConnection(connStr);
-            //try
-            //{
-            //    System.Console.WriteLine("Connecting to MySQL...");
-            //    conn.Open();
-
-            //    string sql = "SELECT * from asd where 1";
-            //    MySqlCommand cmd = new MySqlCommand(sql, conn);
-            //    MySqlDataReader rdr = cmd.ExecuteReader();
-
-            //    while (rdr.Read())
-            //    {
-            //        System.Console.WriteLine(rdr[0] + " -- " );
-            //    }
-            //    rdr.Close();
-            //}
-            //catch (Exception ex)
-            //{
-            //    System.Console.WriteLine(ex.ToString());
-            //}
-
-            //conn.Close();
-            //System.Console.WriteLine("Done.");
+            var client = new MongoClient(new MongoUrl("mongodb://localhost:27017"));
 
 
-
-            //try
-            //{
-            //    System.Console.WriteLine("Connecting to MySQL...");
-            //    conn.Open();
-
-            //    string sql = "SELECT * from  where 1";
-            //    MySqlCommand cmd = new MySqlCommand(sql, conn);
-
-            //    MySqlDataReader rdr = cmd.ExecuteReader();
-
-            //    while (rdr.Read())
-            //    {
-            //        System.Console.WriteLine(rdr[0] + " -- " + rdr[1]);
-            //    }
-            //    rdr.Close();
-            //}
-            //catch (Exception ex)
-            //{
-            //    System.Console.WriteLine(ex.ToString());
-            //}
-
-            //conn.Close();
-            //System.Console.WriteLine("Done.");
+            IMongoDatabase db = client.GetDatabase("projekti2");
+            //System.Console.ReadKey();
+            MainAsync(chain).Wait();
 
 
 
             System.Console.Read();
         }
+
+
+        static async Task MainAsync(BlockChain chain)
+        {
+
+            var client = new MongoClient(new MongoUrl("mongodb://localhost:27017"));
+
+            IMongoDatabase db = client.GetDatabase("projekti2");
+            //var collection = db.GetCollection<BsonDocument>("chain");
+
+            var document = BsonSerializer.Deserialize<BsonDocument>(chain.GetFullChain());
+            var collection = db.GetCollection<BsonDocument>("chain");
+            await collection.InsertOneAsync(document);
+
+            // await collection.InsertOneAsync(document);
+        }
+
     }
 }
