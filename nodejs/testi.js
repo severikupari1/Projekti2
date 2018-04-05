ff67c82642ca9a78f34d737844d3461fafc23ae4
severi <severikupari1@gmail.com>
2018-04-05 14:54:54 +0300

    testi.js toimii haku

---
+++ nodejs/testi.js
@@ -0,0 +1,13 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://10.211.48.117:27017";
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("projekti2");
+    var query = {};
+    dbo.collection("chain").find({}, { _id: 1}).toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result[0].chain[0].Index);
+        db.close();
+      });
+});
