cc0f84f0c3cab21cbeb2597ccca95dc83be44574
Severi <severikupari1@gmail.com>
2018-04-08 13:17:42 +0300

    Search by objectid

--- nodejs/testi.js
+++ nodejs/testi.js
@@ -1,4 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
 //var url = "mongodb://10.211.48.117:27017";
 var url = "mongodb://projekti2:Bloblo1@ds237669.mlab.com:37669/projekti2";
 
@@ -6,7 +7,8 @@ MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     var dbo = db.db("projekti2");
     var query = {};
-    dbo.collection("chain").find({}, { _id: 1}).toArray(function(err, result) {
+    
+    dbo.collection("chain").findOne({"_id": new ObjectId("5ac7ecc0f7b74235f8e9dab0")}, { _id: 1}).toArray(function(err, result) {
         if (err) throw err;
         console.log(result[0].chain.length);
        // var haku = result[0].chain[index].Transactions;
