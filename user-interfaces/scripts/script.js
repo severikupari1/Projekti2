9d2b5a781b306c026dab066b1cddf1f29b3371fe
severi <severikupari1@gmail.com>
2018-04-11 12:17:59 +0300

    cleaned .py

--- user-interfaces/scripts/script.js
+++
@@ -1,8 +0,0 @@
-function Login()
-{
-	var user = document.getElementById('login_username').value;
-	var pass = document.getElementById('login_password').value;
-	
-	// This alert should be replaced with a proper login check
-	alert('You logged in with username "' + user + '" and password "' + pass + '".');
-}
