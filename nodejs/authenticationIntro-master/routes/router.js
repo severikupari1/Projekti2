77ca73cdeb15d5f854f05bfc0a3b162dfba429ab
Severi Cu <severikupari1@gmail.com>
2018-04-17 12:57:32 +0300

    added return to /chain1

--- nodejs/authenticationIntro-master/routes/router.js
+++ nodejs/authenticationIntro-master/routes/router.js
@@ -100,4 +100,33 @@ router.get('/logout', function (req, res, next) {
   }
 });
 
+
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+//var url = "mongodb://10.211.48.117:27017";
+var kokochain;
+var url = "mongodb://projekti2:Bloblo1@ds237669.mlab.com:37669/projekti2";
+
+router.get('/chain1', function (req, res) {
+    
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("projekti2");
+   
+    dbo.collection("chain").find({"_id": new ObjectId("5ac7ecc0f7b74235f8e9dab0")}).toArray(function(err, result) {
+        if (err) throw err;
+        
+       kokochain = result[0];
+       res.send(kokochain);
+        //console.log(result);
+        db.close();
+      });
+});
+
+
+    //console.log(res.kokochain);
+});
+
+
+
 module.exports = router;
