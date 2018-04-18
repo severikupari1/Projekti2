f0e063e62d579eee2ef1c0341a9d8ff73e4d4bf6
Severi <severikupari1@gmail.com>
2018-04-18 20:37:05 +0300

    deleted not needed funtion

--- blockchain-master/csharp/BlockChain.Console/Program.cs
+++ blockchain-master/csharp/BlockChain.Console/Program.cs
@@ -18,20 +18,7 @@ namespace BlockChainDemo.Console
     class Program
     {
 
-        public BsonDocument ReturnBsonDoc()
-        {
-            var Client = new MongoClient(new MongoUrl("mongodb://projekti2:Bloblo1@ds237669.mlab.com:37669/projekti2"));
-            var DB = Client.GetDatabase("projekti2");
-            var collection = DB.GetCollection<BsonDocument>("chain");
-            var filter = new BsonDocument {
-     { "_id" , ObjectId.Parse("5ac7ecc0f7b74235f8e9dab0") } };
-
-            var findOptions = new FindOptions<BsonDocument>();
-            findOptions.Projection = "{'_id': 0}";
-            var hakuchaini = collection.FindSync(filter, findOptions).FirstOrDefault();
-            var document = BsonSerializer.Deserialize<BsonDocument>(hakuchaini);
-            return document;
-        }
+       
 
 
         static void Main(string[] args)
