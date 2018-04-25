2ee76e95de0e51f3d33f7b65dd5a8f703f068231
severi <severikupari1@gmail.com>
2018-04-25 13:24:16 +0300

    Added startschortcut

--- nodejs/StartTrustChain.cmd
+++ nodejs/StartTrustChain.cmd
@@ -1,2 +1,4 @@
+start  ..\blockchain-master\csharp\BlockChain.Console\bin\Debug\BlockChainDemo.Console.exe
 start cmd /c "node.exe .\node_modules\cors-anywhere\server.js"
 start cmd /c "node.exe ..\TrustChainNode\app.js"
+
