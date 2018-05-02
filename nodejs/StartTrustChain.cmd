dd1df68597da734a4a4cf35f60c3e651e0b9302e
severi <severikupari1@gmail.com>
2018-05-02 11:43:53 +0300

    fixed paths

--- nodejs/StartTrustChain.cmd
+++ nodejs/StartTrustChain.cmd
@@ -1,4 +1,4 @@
 start  .\blockchain-master\csharp\BlockChain.Console\bin\Debug\BlockChainDemo.Console.exe
-start cmd /c "node.exe .\nodejs\node_modules\cors-anywhere\server.js"
-start cmd /c "node.exe .\nodejs\TrustChainNode\app.js"
+start cmd /c ".\nodejs\node.exe .\nodejs\node_modules\cors-anywhere\server.js"
+start cmd /c ".\nodejs\node.exe .\TrustChainNode\app.js"
 
